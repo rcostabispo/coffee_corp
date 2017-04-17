02ae1bf30338a5f202097bd8d3ac467271c2f995
Ronaldo da Costa Bispo <rcosta.bispo@gmail.com.br>
2017-04-17 18:21:19 -0300

    Adds Product model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,7 @@
 # Users
 User.create(name: 'Ronaldo da Costa Bispo', document: '300.001.788-89', phone: '14999999999', status: :active, notes: 'Possui todas as permissões no sistema', email: 'rcosta.bispo@gmail.com', password: '123456', admin: true)
 User.create(name: 'Usuario comum', document: '111.222.333-44', phone: '14988888888', status: :active, notes: 'Possui somente algumas permissões no sistema', email: 'user.default@gmail.com', password: '123456')
+
+# Products
+Product.create(description: 'Açucar Cristal', notes: 'Somente nas cores brancas e amarela. Light não!!')
+Product.create(description: 'Malto', notes: 'Acima de 60Kg')
